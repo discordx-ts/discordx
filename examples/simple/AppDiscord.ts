56a228c3503e7b6e187204a949aaf9683184d3eb
Owen Gombas <owen.gombas@gmail.com>
2019-06-23 11:35:42 +0200

    update(method name)

--- examples/simple/AppDiscord.ts
+++ examples/simple/AppDiscord.ts
@@ -1,10 +1,14 @@
-import { Discord, On, Client } from "../../src";
-import { Message } from "discord.js";
+import {
+  Discord,
+  On,
+  Client // Use the Client that are provided by @typeit/discord
+} from "../../src";
+// You must import the types from @types/discord.js
+import {
+  Message
+} from "discord.js";
 
-enum Commands {
-  HELLO = "hello"
-}
-
+// Decorate the class with the @Discord decorator
 @Discord
 export class AppDiscord {
   private static _client: Client;
@@ -14,19 +18,23 @@ export class AppDiscord {
 
   static start() {
     this._client = new Client();
+    // In the login method, you must specify the glob string to load your classes (for the framework).
+    // In this case that's not necessary because the entry point of your application is this file.
     this._client.login(
       "YOUR_TOKEN",
-      `${__dirname}/*Discord.ts`
+      `${__dirname}/*Discord.ts` // glob string to load the classes
     );
   }
 
+  // When the "message" event is triggered, this method is called with a specific payload (related to the event)
   @On("message")
   async onMessage(message: Message, client: Client) {
+    // Your logic...
     if (AppDiscord._client.user.id !== message.author.id) {
       if (message.content[0] === this._prefix) {
         const cmd = message.content.replace(this._prefix, "").toLowerCase();
         switch (cmd) {
-          case Commands.HELLO:
+          case "hello":
             message.reply(this._sayHelloMessage);
             break;
           default:
@@ -38,4 +46,5 @@ export class AppDiscord {
   }
 }
 
+// Start your app
 AppDiscord.start();
