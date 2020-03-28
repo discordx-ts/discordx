75db01177b50a2af728219c9aab149e70378bd09
owen <owen@venmac.local>
2020-03-28 11:32:56 +0100

    update(examples)

--- examples/multipleDiscordInstances/Main.ts
+++ examples/multipleDiscordInstances/Main.ts
@@ -12,7 +12,7 @@ export class Main {
     // In the login method, you must specify the glob string to load your classes (for the framework).
     // In this case that's not necessary because the entry point of your application is this file.
     this._client.login(
-      "Njg4NDY2MTk0MDA5MDMwNzAy.Xn5USw.L0xWocEaCXHX6gcp155VEkwR6Ok",
+      "YOUR_TOKEN",
       `${__dirname}/Discords/*.ts` // glob string to load the classes
     );
   }
--- examples/simple/AppDiscord.ts
+++ examples/simple/AppDiscord.ts
@@ -33,7 +33,7 @@ export class AppDiscord {
     // In the login method, you must specify the glob string to load your classes (for the framework).
     // In this case that's not necessary because the entry point of your application is this file.
     this._client.login(
-      "Njg4NDY2MTk0MDA5MDMwNzAy.Xn5USw.L0xWocEaCXHX6gcp155VEkwR6Ok",
+      "YOUR_TOKEN",
       `${__dirname}/*Discord.ts` // glob string to load the classes
     );
   }
