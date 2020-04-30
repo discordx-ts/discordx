05ef26c71a31084bdfdf318d95a0606083d8cac0
owen <owen@venmac.local>
2020-04-30 15:25:03 +0200

    feature(importCommands)

--- src/Decorators/Once.ts
+++ src/Decorators/Once.ts
@@ -5,16 +5,18 @@ import {
 
 export function Once(event: DiscordEvent);
 export function Once(event: string);
-export function Once(event: DiscordEvent | string) {
+export function Once(event: DiscordEvent) {
   return (target: Object, key: string, descriptor: PropertyDescriptor): void => {
     MetadataStorage.Instance.AddOn({
       class: target.constructor,
       key,
       params: {
+        from: target.constructor,
         guards: [],
         event,
         once: true,
-        method: descriptor.value
+        method: descriptor.value,
+        originalParams: {}
       }
     });
   };
