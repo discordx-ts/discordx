6f65cf09e1ce25232cf3b6224885307c043edb38
owen <owen@venmac.local>
2020-05-19 21:17:03 +0200

    feature(args parsing)

--- examples/multiple-discord-instances/Main.ts
+++ examples/multiple-discord-instances/Main.ts
@@ -13,7 +13,7 @@ export class Main {
     // In the login method, you must specify the glob string to load your classes (for the framework).
     // In this case that's not necessary because the entry point of your application is this file.
     this._client.login(
-      "NzExNzUyMTU2Mjg5Njk1Nzk2.XsHk3w.-Ifb_17Zu787EbqdEdC_Seo3-V4",
+      "YOUR_TOKEN",
       `${__dirname}/discords/*.ts`, // glob string to load the classes
       `${__dirname}/discords/*.js` // If you compile your bot, the file extension will be .js
     );
