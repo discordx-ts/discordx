710686380306ba867130af28de091282d096c8ff
owen <owen@venmac.local>
2020-05-20 00:44:43 +0200

    example(simple-commands)

---
+++ examples/simple-commands/Main.ts
@@ -0,0 +1,25 @@
+import { Client } from "../../src";
+
+export class Main {
+  private static _client: Client;
+
+  static get Client(): Client {
+    return this._client;
+  }
+
+  static start() {
+    this._client = new Client();
+
+    // In the login method, you must specify the glob string to load your classes (for the framework).
+    // In this case that's not necessary because the entry point of your application is this file.
+    this._client.login(
+      "YOUR_TOKEN",
+      `${__dirname}/discords/*.ts`, // glob string to load the classes
+      `${__dirname}/discords/*.js` // If you compile your bot, the file extension will be .js
+    );
+
+    console.log(Client.getCommands());
+  }
+}
+
+Main.start();
---
+++ examples/simple-commands/discords/AppDiscord.ts
@@ -0,0 +1,23 @@
+import {
+  Discord,
+  CommandMessage,
+  Command,
+  Description,
+  Infos
+} from "../../../src";
+
+interface HelloArgs {
+  slug: string;
+  number: number;
+}
+
+@Discord("!")
+@Description("My super app")
+@Infos({ hello: "world" })
+export abstract class AppDiscord {
+  @Command("hello :slug :number")
+  hello(command: CommandMessage<HelloArgs>) {
+    const { slug, number } = command.args;
+    console.log(slug, number);
+  }
+}
