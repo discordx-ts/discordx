8931071ed30e2b913ef15e014807817bc36b2c0f
owencalvin <owen.gombas@gmail.com>
2021-05-20 21:38:24 +0200

    init

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
    */
   description: description,
 
-  base: "/graphql-composer-decorators/",
+  base: "/discord.ts/",
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
