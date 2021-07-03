8c5fca1451ea2927828f4bef21d306cb5b896848
iorp <indianoceanroleplay@gmail.com>
2021-07-03 23:09:29 +0530

    added menu example

---
+++ examples/menu/discords/AppDiscord.ts
@@ -0,0 +1,54 @@
+import {
+  CommandInteraction,
+  MessageActionRow,
+  SelectMenuInteraction,
+  MessageSelectMenu,
+} from "discord.js";
+import { Discord, Slash, SelectMenu } from "../../../src";
+
+const roles = [
+  { label: "Principal", value: "principal" },
+  { label: "Teacher", value: "teacher" },
+  { label: "Student", value: "student" },
+];
+
+@Discord()
+export abstract class buttons {
+  @SelectMenu("role-menu")
+  async handle(interaction: SelectMenuInteraction): Promise<unknown> {
+    await interaction.defer();
+
+    // extract selected value by member
+    const roleValue = interaction.values?.[0];
+
+    // if value not found
+    if (!roleValue)
+      return await interaction.followUp("invalid role id, select again");
+    await interaction.followUp(
+      `you have selected role: ${
+        roles.find((r) => r.value === roleValue).label
+      }`
+    );
+    return;
+  }
+
+  @Slash("myroles", { description: "roles menu" })
+  async button(interaction: CommandInteraction): Promise<unknown> {
+    await interaction.defer();
+
+    // create menu for roels
+    const menu = new MessageSelectMenu()
+      .addOptions(roles)
+      .setCustomID("role-menu");
+
+    // create a row for meessage actions
+    const buttonRow = new MessageActionRow().addComponents(menu);
+
+    // send it
+    interaction.editReply({
+      content: "select your role!",
+      components: [buttonRow],
+    });
+    return;
+  }
+}
