cc435c43ba7d707ba13247f028b6f3fc692a2ef4
iorp <indianoceanroleplay@gmail.com>
2021-07-10 02:04:09 +0530

    doc update

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: "discord.ts official documentation",
+  title: "discordx or discord.ts official documentation",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
