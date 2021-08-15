e02f3d8437615935cbbd7ff73f450f000ac69d38
oceanroleplay <indianoceanroleplay@gmail.com>
2021-08-16 02:46:42 +0530

    doc link for ArgsOf

--- src/types/public/ArgsOf.ts
+++ src/types/public/ArgsOf.ts
@@ -2,5 +2,7 @@ import { ClientEvents } from "discord.js";
 
 /**
  * Type the arguments of an event
+ * ___
+ * [View Documentation](https://oceanroleplay.github.io/discord.ts/docs/general/argsof)
  */
 export type ArgsOf<K extends keyof ClientEvents> = ClientEvents[K];
