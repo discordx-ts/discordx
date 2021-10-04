f9a919605beda47f164e878a1a68e1ae034c620d
VictoriqueMoe <loli@victorique.moe>
2021-10-04 01:34:45 +0100

    Update AppDiscord.ts
    Update to remove `@singleton` and add `@injectable()` and also use container.resolve instead of DisxcordX internal one.
    This change also reflexts the documentation i made in PR https://github.com/oceanroleplay/discord.ts/pull/96

--- examples/tsyringe/discords/AppDiscord.ts
+++ examples/tsyringe/discords/AppDiscord.ts
@@ -1,46 +1,47 @@
-import { DIService, Discord, Slash } from "../../../src";
-import { CommandInteraction } from "discord.js";
-import { singleton } from "tsyringe";
+import {Discord, DIService, Slash} from "../../../src";
+import {CommandInteraction} from "discord.js";
+import {container, injectable, singleton} from "tsyringe";
 
 @singleton()
 class Database {
-  database: string;
-  constructor() {
-    console.log("I am database");
-    this.database = "connected";
-  }
+    database: string;
 
-  query() {
-    return this.database;
-  }
+    constructor() {
+        console.log("I am database");
+        this.database = "connected";
+    }
+
+    query() {
+        return this.database;
+    }
 }
 
 @Discord()
-@singleton() // singleton must defined under @Discord
+@injectable()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class AppDiscord {
-  constructor(private database: Database) {
-    // I am just a empty constructor :(
-  }
-
-  @Slash("tsyringe")
-  tsyringe(interaction: CommandInteraction): void {
-    if (DIService.container) {
-      const myClass = DIService.container.resolve(AppDiscord);
-      interaction.reply(
-        `${myClass.database.query()}, same class: ${myClass === this}`
-      );
-    } else {
-      interaction.reply("Not using tsyringe");
+    constructor(private database: Database) {
+        // I am just a empty constructor :(
     }
-  }
 
-  @Slash("tsyringe2")
-  tsyringe2(interaction: CommandInteraction): void {
-    if (DIService.container) {
-      interaction.reply(this.database.query());
-    } else {
-      interaction.reply("Not using tsyringe");
+    @Slash("tsyringe")
+    tsyringe(interaction: CommandInteraction): void {
+        if (DIService.container) {
+            const myClass = container.resolve(AppDiscord);
+            interaction.reply(
+                `${myClass.database.query()}, same class: ${myClass === this}`
+            );
+        } else {
+            interaction.reply("Not using tsyringe");
+        }
     }
-  }
+
+    @Slash("tsyringe2")
+    tsyringe2(interaction: CommandInteraction): void {
+        if (DIService.container) {
+            interaction.reply(this.database.query());
+        } else {
+            interaction.reply("Not using tsyringe");
+        }
+    }
 }
