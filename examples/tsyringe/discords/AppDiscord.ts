a51658b0eba0182e7cae7f5e5fdeccf811557271
Indian Ocean Roleplay <indianoceanroleplay@gmail.com>
2021-10-04 09:39:36 +0530

    Update AppDiscord.ts

--- examples/tsyringe/discords/AppDiscord.ts
+++ examples/tsyringe/discords/AppDiscord.ts
@@ -1,47 +1,47 @@
-import {Discord, DIService, Slash} from "../../../src";
-import {CommandInteraction} from "discord.js";
-import {container, injectable, singleton} from "tsyringe";
+import { DIService, Discord, Slash } from "../../../src";
+import { container, injectable, singleton } from "tsyringe";
+import { CommandInteraction } from "discord.js";
 
 @singleton()
 class Database {
-    database: string;
+  database: string;
 
-    constructor() {
-        console.log("I am database");
-        this.database = "connected";
-    }
+  constructor() {
+    console.log("I am database");
+    this.database = "connected";
+  }
 
-    query() {
-        return this.database;
-    }
+  query() {
+    return this.database;
+  }
 }
 
 @Discord()
 @injectable()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class AppDiscord {
-    constructor(private database: Database) {
-        // I am just a empty constructor :(
-    }
+  constructor(private database: Database) {
+    // I am just a empty constructor :(
+  }
 
-    @Slash("tsyringe")
-    tsyringe(interaction: CommandInteraction): void {
-        if (DIService.container) {
-            const myClass = container.resolve(AppDiscord);
-            interaction.reply(
-                `${myClass.database.query()}, same class: ${myClass === this}`
-            );
-        } else {
-            interaction.reply("Not using tsyringe");
-        }
+  @Slash("tsyringe")
+  tsyringe(interaction: CommandInteraction): void {
+    if (DIService.container) {
+      const myClass = container.resolve(AppDiscord);
+      interaction.reply(
+        `${myClass.database.query()}, same class: ${myClass === this}`
+      );
+    } else {
+      interaction.reply("Not using tsyringe");
     }
+  }
 
-    @Slash("tsyringe2")
-    tsyringe2(interaction: CommandInteraction): void {
-        if (DIService.container) {
-            interaction.reply(this.database.query());
-        } else {
-            interaction.reply("Not using tsyringe");
-        }
+  @Slash("tsyringe2")
+  tsyringe2(interaction: CommandInteraction): void {
+    if (DIService.container) {
+      interaction.reply(this.database.query());
+    } else {
+      interaction.reply("Not using tsyringe");
     }
+  }
 }
