1ea6a8e716ec2d50c3db29e51c818e17616542b5
oceanroleplay <indianoceanroleplay@gmail.com>
2021-10-22 06:35:33 +0530

    update GuildNotFoundError.ts

--- src/classes/errors/GuildNotFound.ts
+++ src/classes/errors/GuildNotFound.ts
@@ -1,7 +1,7 @@
 import { Snowflake } from "discord.js";
 
 export class GuildNotFoundError extends Error {
-  constructor(guildID: Snowflake) {
-    super(`Your bot is not in the guild: ${guildID}`);
+  constructor(guildId: Snowflake) {
+    super(`Your bot is not in the guild: ${guildId}`);
   }
 }
