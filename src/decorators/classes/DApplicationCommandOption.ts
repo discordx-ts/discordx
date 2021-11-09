bb39f047cd6195c8366f4a5c61f17788f613c972
oceanroleplay <indianoceanroleplay@gmail.com>
2021-11-09 18:12:22 +0530

    refactor: remove undefined from channel types

--- src/decorators/classes/DApplicationCommandOption.ts
+++ src/decorators/classes/DApplicationCommandOption.ts
@@ -1,3 +1,4 @@
+import * as _ from "lodash";
 import {
   DApplicationCommandOptionChoice,
   SlashAutoCompleteOption,
@@ -135,34 +136,35 @@ export class DApplicationCommandOption extends Decorator {
     | "GUILD_PUBLIC_THREAD"
     | "GUILD_PRIVATE_THREAD"
     | "GUILD_STAGE_VOICE"
-    | undefined
   )[] {
-    return this.channelTypes.map((ch) => {
-      switch (ch) {
-        case 0:
-          return "GUILD_TEXT";
-        case 1:
-          return "DM";
-        case 2:
-          return "GUILD_VOICE";
-        case 3:
-          return "GROUP_DM";
-        case 4:
-          return "GUILD_CATEGORY";
-        case 5:
-          return "GUILD_NEWS";
-        case 6:
-          return "GUILD_STORE";
-        case 10:
-          return "GUILD_NEWS_THREAD";
-        case 11:
-          return "GUILD_PUBLIC_THREAD";
-        case 12:
-          return "GUILD_PRIVATE_THREAD";
-        case 13:
-          return "GUILD_STAGE_VOICE";
-      }
-    });
+    return _.compact(
+      this.channelTypes.map((ch) => {
+        switch (ch) {
+          case 0:
+            return "GUILD_TEXT";
+          case 1:
+            return "DM";
+          case 2:
+            return "GUILD_VOICE";
+          case 3:
+            return "GROUP_DM";
+          case 4:
+            return "GUILD_CATEGORY";
+          case 5:
+            return "GUILD_NEWS";
+          case 6:
+            return "GUILD_STORE";
+          case 10:
+            return "GUILD_NEWS_THREAD";
+          case 11:
+            return "GUILD_PUBLIC_THREAD";
+          case 12:
+            return "GUILD_PRIVATE_THREAD";
+          case 13:
+            return "GUILD_STAGE_VOICE";
+        }
+      })
+    );
   }
 
   toJSON(config?: { channelString?: boolean }): ApplicationCommandOptionData {
@@ -176,7 +178,7 @@ export class DApplicationCommandOption extends Decorator {
         this.channelTypes.length === 0
           ? undefined
           : config?.channelString
-          ? (this.channelTypesEx() as unknown as undefined)
+          ? this.channelTypesEx()
           : this.channelTypes,
       choices:
         this.choices.length === 0
