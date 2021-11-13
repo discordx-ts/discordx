641818b6d30e54f97bd86b855a68042bcf3520df
oceanroleplay <indianoceanroleplay@gmail.com>
2021-11-13 06:58:07 +0530

    chore: prepare ts4.5

---
+++ scripts/postbuild.mjs
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/quotes */
+import fs from "fs";
+import glob from "glob";
+
+function argPath(search) {
+  const findPathArg = process.argv.find((arg) => arg.includes(`--${search}=`));
+  if (!findPathArg) {
+    return undefined;
+  }
+
+  return findPathArg.replace(`--${search}=`, "");
+}
+
+const path = argPath("path");
+const module = argPath("module");
+
+if (!path || !module) {
+  console.log("--path, --module are required args");
+} else if (module !== "cjs" && module !== "esm") {
+  console.log("Invalid module, valid values are: cjs, esm");
+} else {
+  const jsExt = module === "cjs" ? ".cjs" : ".mjs";
+  const tsExt = module === "cjs" ? ".cts" : ".mts";
+  const files = glob.sync(path + "/**/*.{js,d.ts,js.map}");
+  files.forEach((file) => {
+    const data = fs.readFileSync(file, {
+      encoding: "utf-8",
+    });
+    fs.rmSync(file);
+    fs.writeFileSync(
+      file.replaceAll(/\.js/gm, jsExt).replaceAll(/\.ts/gm, tsExt),
+      data
+        .replaceAll(/\.js/gm, jsExt)
+        .replaceAll('"' + "discord" + jsExt + '"', '"discord.js"')
+    );
+  });
+
+  console.log("postbuild completed...");
+}
