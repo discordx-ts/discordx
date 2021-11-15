7d92cdabc44225feb5cd8d1393e953b8db711898
oceanroleplay <indianoceanroleplay@gmail.com>
2021-11-15 17:55:46 +0530

    refactor: type literal

--- src/types/core/literal.ts
+++ src/types/core/literal.ts
@@ -1,15 +1,4 @@
-export type ForbidCharacter<
-  S extends string,
-  Character extends string
-> = S extends `${string}${Character}${string}` ? never : unknown;
-
-export type WhitelistWords<S, D extends string> = S extends ""
-  ? unknown
-  : S extends `${D}${infer Tail}`
-  ? WhitelistWords<Tail, D>
-  : never;
-
-type SpecialCharacters =
+export type SpecialCharacters =
   | "~"
   | "`"
   | "!"
@@ -43,17 +32,29 @@ type SpecialCharacters =
   | "?"
   | " ";
 
-type TruncateTo32<T extends string> = T extends ""
-  ? never
+export type ForbidCharacter<
+  S extends string,
+  Character extends string
+> = S extends `${string}${Character}${string}` ? never : S;
+
+export type WhitelistWords<S, D extends string> = S extends ""
+  ? unknown
+  : S extends `${D}${infer Tail}`
+  ? WhitelistWords<Tail, D>
+  : never;
+
+export type TruncateTo32<T extends string> = T extends ""
+  ? unknown
   : T extends `${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}${infer R}`
   ? T extends `${infer F}${R}`
     ? F
     : never
   : T;
 
-export type VName<S extends string> = S &
+export type NotEmpty<T> = T extends "" ? never : unknown;
+
+export type VName<S extends string> = NotEmpty<S> &
   Lowercase<S> &
-  Required<S> &
   TruncateTo32<S> &
   ForbidCharacter<S, SpecialCharacters>;
 
