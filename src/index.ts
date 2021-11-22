b9dcfe28de74a49760a3234618426df083877dd6
oceanroleplay <indianoceanroleplay@gmail.com>
2021-11-22 19:27:13 +0530

    chore: repo cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import "reflect-metadata";
 export * from "./logic/index.js";
 export * from "./decorators/index.js";
 export * from "./types/index.js";
-export * from "./client.js";
+export * from "./Client.js";
 export * from "./classes/index.js";
 export * from "./util/index.js";
