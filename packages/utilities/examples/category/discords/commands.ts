8fa635390c353f8af909208554848590f9609743
oceanroleplay <indianoceanroleplay@gmail.com>
2021-12-29 19:43:26 +0530

    chore: format example

--- packages/utilities/examples/category/discords/commands.ts
+++ packages/utilities/examples/category/discords/commands.ts
@@ -1,12 +1,12 @@
-import { CommandInteraction } from "discord.js";
 import { Discord, Slash } from "discordx";
 import { Category } from "../../../build/cjs/v2";
+import { CommandInteraction } from "discord.js";
 
 @Discord()
 @Category("Admin Commands")
 export abstract class SlashExample {
   @Slash("testx")
-  async testx(interaction: CommandInteraction): Promise<void> {
+  testx(interaction: CommandInteraction): void {
     interaction.reply("Hey!");
   }
 }
