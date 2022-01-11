052c5e2fde468e7b121d524582c9c36361695dde
oceanroleplay <indianoceanroleplay@gmail.com>
2022-01-11 22:31:20 +0530

    fix: koa esm import

--- packages/koa/src/decorators/index.ts
+++ packages/koa/src/decorators/index.ts
@@ -2,6 +2,6 @@ export * from "./classes/DRequest.js";
 export * from "./classes/DRouter.js";
 
 export * from "./decorators/Api.js";
-export * from "./decorators/Middleware";
+export * from "./decorators/Middleware.js";
 export * from "./decorators/Request.js";
 export * from "./decorators/Router.js";
