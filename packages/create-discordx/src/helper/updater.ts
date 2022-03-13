825c4b01f7715c1c53fcffb528e47ba7cd825801
oceanroleplay <indianoceanroleplay@gmail.com>
2022-03-13 07:32:24 +0530

    chore: improve update check

--- packages/create-discordx/src/helper/updater.ts
+++ packages/create-discordx/src/helper/updater.ts
@@ -1,3 +1,6 @@
+import boxen from "boxen";
+import chalk from "chalk";
+import isInstalledGlobally from "is-installed-globally";
 import { readFile } from "node:fs/promises";
 import checkForUpdate from "update-check";
 
@@ -17,9 +20,38 @@ let update = null;
 try {
   update = await checkForUpdate(packageJson);
 } catch (err) {
-  console.error(`Failed to check for updates: ${err}`);
+  console.log(
+    boxen("Failed to check for updates", {
+      align: "center",
+      borderColor: "red",
+      borderStyle: "round",
+      margin: 1,
+      padding: 1,
+    })
+  );
 }
 
 if (update) {
-  console.log(`The latest version is ${update.latest}. Please update!`);
+  const updateCmd = isInstalledGlobally
+    ? "npm i -g create-discordx@latest"
+    : "npm i create-discordx@latest";
+
+  const template =
+    "Update available " +
+    chalk.dim(`${packageJson.version}`) +
+    chalk.reset(" → ") +
+    chalk.green(`${update.latest}`) +
+    " \nRun " +
+    chalk.cyan(updateCmd) +
+    " to update";
+
+  console.log(
+    boxen(template, {
+      align: "center",
+      borderColor: "yellow",
+      borderStyle: "round",
+      margin: 1,
+      padding: 1,
+    })
+  );
 }
