b393959015e1a84c35924805064256e7481e969a
oceanroleplay <indianoceanroleplay@gmail.com>
2022-04-01 05:22:06 +0530

    docs: add github redirects

---
+++ docs/src/pages/github/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function Discord() {
+  React.useEffect(() => {
+    window.location.href = "https://github.com/oceanroleplay/discord.ts";
+  }, []);
+  return null;
+}
---
+++ docs/src/pages/github/tree/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function Redirect() {
+  React.useEffect(() => {
+    window.location.href =
+      "https://github.com/oceanroleplay/discord.ts/tree/main";
+  }, []);
+  return null;
+}
