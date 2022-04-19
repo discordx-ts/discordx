bc07cfae39c70ba68f3fbf9c5659331e578a29a7
Satont <satontworldwide@gmail.com>
2022-04-19 19:56:49 +0300

    refactor: use undici fetch instead of axios and got

--- packages/create-discordx/src/helper/template.ts
+++ packages/create-discordx/src/helper/template.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
-import got from "got";
-import { Stream } from "stream";
+import { Readable, Stream } from "stream";
 import tar from "tar";
+import { fetch } from "undici";
 import { promisify } from "util";
 
 /**
@@ -12,35 +11,11 @@ import { promisify } from "util";
 export async function GetTemplates(): Promise<
   { title: string; value: string }[]
 > {
-  const response = await axios
-    .get<{ name: string; path: string; type: string }[]>(
-      "https://api.github.com/repos/oceanroleplay/discordx-templates/contents"
-    )
-    .then((res) =>
-      res.data
-        .filter((row) => row.type === "dir" && /^[0-9].+/.test(row.name))
-        .map((row) => ({ title: row.name, value: row.path }))
-    )
-    .catch(() => []);
+  const request = await fetch("https://api.github.com/repos/oceanroleplay/discordx-templates/contents");
+  const response = await request.json() as Array<{ name: string; path: string; type: string }>;
 
-  return response;
-}
-
-/**
- * Check if the template exists on GitHub
- *
- * @param name template name
- * @returns
- */
-export async function IsTemplateExist(name: string): Promise<boolean> {
-  const response = await axios
-    .get(
-      `https://api.github.com/repos/oceanroleplay/discordx-templates/contents/${name}?ref=main`
-    )
-    .then(() => true)
-    .catch(() => false);
-
-  return response;
+  return response.filter((row) => row.type === "dir" && /^[0-9].+/.test(row.name))
+    .map((row) => ({ title: row.name, value: row.path }));
 }
 
 /**
@@ -50,16 +25,17 @@ export async function IsTemplateExist(name: string): Promise<boolean> {
  * @param name project name
  * @returns
  */
-export function DownloadAndExtractTemplate(
+export async function DownloadAndExtractTemplate(
   root: string,
   name: string
 ): Promise<void> {
   const pipeline = promisify(Stream.pipeline);
 
+  const request = await fetch("https://codeload.github.com/oceanroleplay/discordx-templates/tar.gz/main");
+  const readableWebStream = request.body as AsyncIterable<unknown>;
+
   return pipeline(
-    got.stream(
-      "https://codeload.github.com/oceanroleplay/discordx-templates/tar.gz/main"
-    ),
+    Readable.from(readableWebStream),
     tar.extract({ cwd: root, strip: 2 }, [`discordx-templates-main/${name}`])
   );
 }
