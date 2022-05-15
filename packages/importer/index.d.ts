1ba31033961c7a5ff1b8ef0aa134e1bf1ab6ff0d
oceanroleplay <indianoceanroleplay@gmail.com>
2022-05-15 19:37:56 +0530

    fix: importer

---
+++ packages/importer/index.d.ts
@@ -0,0 +1,4 @@
+export declare const isESM: boolean;
+export declare function dirname(url: string): string;
+export declare function resolve(...paths: string[]): string[];
+export declare function importx(...paths: string[]): Promise<void>;
