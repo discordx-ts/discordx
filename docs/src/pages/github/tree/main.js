fa5afd7a778fd7844470c979040efdfadc9f6b1a
oceanroleplay <indianoceanroleplay@gmail.com>
2022-08-18 12:21:39 +0530

    chore: format

--- docs/src/pages/github/tree/main.js
+++ docs/src/pages/github/tree/main.js
@@ -3,8 +3,7 @@ import React from "react";
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function Redirect() {
   React.useEffect(() => {
-    window.location.href =
-      "https://github.com/discordx-ts/discordx/tree/main";
+    window.location.href = "https://github.com/discordx-ts/discordx/tree/main";
   }, []);
   return null;
 }
