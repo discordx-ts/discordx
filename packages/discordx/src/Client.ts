a34ac74ed56c0c41549dd73f1394b7fa7b2179e4
oceanroleplay <indianoceanroleplay@gmail.com>
2022-09-13 21:59:53 +0530

    refactor: use for instead map

--- packages/discordx/src/Client.ts
+++ packages/discordx/src/Client.ts
@@ -1433,7 +1433,7 @@ export class Client extends ClientJS {
       this.printDebug();
     }
 
-    this.instance.usedEvents.map((on) => {
+    for (const on of this.instance.usedEvents) {
       if (on.rest) {
         if (on.once) {
           this.rest.once(
@@ -1483,6 +1483,6 @@ export class Client extends ClientJS {
           );
         }
       }
-    });
+    }
   }
 }
