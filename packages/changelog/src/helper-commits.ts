ba4058aa86db9b8b5ffe35982599ece91359e803
Indian Ocean Roleplay <indianoceanroleplay@gmail.com>
2022-09-22 15:46:19 +0530

    refactor(changelog): code improvement (#871)
    BREAKING-CHANGE: code wise improvement

---
+++ packages/changelog/src/helper-commits.ts
@@ -0,0 +1,85 @@
+import child from "child_process";
+
+import type { CommitType } from "./metadata.js";
+import { CommitCategories } from "./metadata.js";
+
+export type Commit = {
+  message: string;
+  sha: string;
+  title: string;
+  type: CommitType;
+};
+
+function FormatTitle(options: {
+  replacement?: CommitType;
+  repo: string;
+  sha: string;
+  title: string;
+}) {
+  return `* ${options.title.replace(
+    !options.replacement
+      ? ""
+      : new RegExp(`${options.replacement}\\(.*\\): |${options.replacement}: `),
+    ""
+  )} ([${options.sha.substring(0, 6)}](${options.repo}/commit/${
+    options.sha
+  }))\n`;
+}
+
+export function GetCommits(config: {
+  from: string;
+  path: string;
+  repo: string;
+  to: string;
+}): Commit[] {
+  const tagDiff =
+    config.from === config.to
+      ? config.to
+      : `${config.from}..${config.to === "head" ? "" : config.to}`;
+
+  const commits = child
+    .execSync(
+      `git log ${tagDiff} --format=%B----SEPARATOR----%H----DELIMITER---- ${config.path}`
+    )
+    .toString("utf-8")
+    .split("----DELIMITER----\n")
+    .map((commit): Commit | null => {
+      const [message, sha] = commit.split("----SEPARATOR----");
+
+      const title = message
+        ?.split("\n")[0]
+        ?.replaceAll(/#([0-9]{1,})/gm, `[#$1](${config.repo}/issues/$1)`);
+
+      if (!message || !sha || !title) {
+        return null;
+      }
+
+      const category = CommitCategories.find((cat) =>
+        cat.types.some((subType) =>
+          new RegExp(`${subType}\\(.*\\): |${subType}: `).test(title)
+        )
+      );
+
+      const type = category?.types.find((subType) =>
+        new RegExp(`${subType}\\(.*\\): |${subType}: `).test(title)
+      );
+
+      return {
+        message: message,
+        sha: sha,
+        title: FormatTitle({
+          replacement: type,
+          repo: config.repo,
+          sha: sha,
+          title: title,
+        }),
+        type:
+          category?.breaking && message.includes("BREAKING CHANGE")
+            ? "BREAKING CHANGE"
+            : type ?? "untagged",
+      };
+    })
+    .filter((tag): tag is Commit => tag !== null);
+
+  return commits;
+}
--- packages/changelog/src/index.ts
+++ packages/changelog/src/index.ts
@@ -1,215 +1,80 @@
-import child from "child_process";
 import fs from "fs";
-import _ from "lodash";
 import prettier from "prettier";
 
-export type storeType =
-  | "build"
-  | "chore"
-  | "ci"
-  | "docs"
-  | "feat"
-  | "feature"
-  | "fix"
-  | "refactor"
-  | "revert"
-  | "test"
-  | "types"
-  | "workflow"
-  | "untagged"
-  | "BREAKING CHANGE";
+import { GetCommits } from "./helper-commits.js";
+import { GetRepoUrl } from "./helper-git.js";
+import { GetTagDate, GetTags } from "./helper-tag.js";
+import { CommitCategories } from "./metadata.js";
 
-const categories: {
-  breaking: boolean;
-  storeTypes: storeType[];
-  title: string;
-}[] = [
-  {
-    breaking: false,
-    storeTypes: ["BREAKING CHANGE"],
-    title: "Breaking Changes",
-  },
-  { breaking: false, storeTypes: ["feat", "feature"], title: "Features" },
-  { breaking: true, storeTypes: ["refactor"], title: "Changed" },
-  { breaking: true, storeTypes: ["fix"], title: "Fixed" },
-  { breaking: false, storeTypes: ["revert"], title: "Reverts" },
-  { breaking: false, storeTypes: ["test"], title: "Tests" },
-  { breaking: false, storeTypes: ["build"], title: "Build" },
-  { breaking: false, storeTypes: ["types"], title: "Types" },
-  { breaking: false, storeTypes: ["docs"], title: "Documentation" },
-  { breaking: false, storeTypes: ["chore"], title: "Routine Tasks" },
-  { breaking: false, storeTypes: ["ci", "workflow"], title: "CI" },
-  { breaking: false, storeTypes: ["untagged"], title: "Untagged" },
-];
+type Options = {
+  matchTag: string; // "v*"
+  onlyStage: boolean;
+  out: string; // "./CHANGELOG.md"
+  replaceTag?: string;
+  src: string; // "./"
+};
 
-export function getRepoUrl(): string {
-  let remoteUrl = child
-    .execSync("git config --get remote.origin.url")
-    .toString("utf-8")
-    .trim();
+export function GenerateChangelog(config: Options): {
+  content: string;
+  repo: string;
+} {
+  const repo = GetRepoUrl();
+  let content = "";
 
-  if (!remoteUrl.length) {
-    throw Error(
-      "repo not found, make sure to run the command in the git directory only"
-    );
-  }
+  const allTags = GetTags(config.matchTag);
 
-  if (!remoteUrl.endsWith(".git")) {
-    remoteUrl += ".git";
-  }
-
-  if (RegExp(/.*@.*:.*.git/gm).test(remoteUrl)) {
-    const regExp = RegExp(/.*@(.*?):(.*?).git/gm).exec(remoteUrl);
-    if (!regExp) {
-      throw Error("Invalid repo url passed");
-    }
-
-    const domain = regExp.at(1);
-    const subUrl = regExp.at(2);
-
-    return `https://${domain ?? ""}/${subUrl ?? ""}`;
-  }
-
-  return remoteUrl.substring(0, remoteUrl.length - 4);
-}
-
-export function generateDoc(options?: {
-  header?: string;
-  ignoreScopes?: string[];
-  outDir?: string;
-  root?: string;
-  tag?: {
-    match?: string;
-    onlyStage?: boolean;
-    replace?: string;
-  };
-}): string {
-  const ignoreScopes: string[] = options?.ignoreScopes
-    ? options.ignoreScopes
-    : [];
-  const repo = getRepoUrl();
-
-  let completeChangelog = "";
-
-  const tags = _.compact(
-    child
-      .execSync(
-        `git tag --list --sort=v:refname "${options?.tag?.match ?? "v*"}"`
-      )
-      .toString("utf-8")
-      .split("\n")
-  );
-
-  tags.push("head");
-
-  if (!tags.length) {
-    throw Error("Without tags, changelogs could not be generated");
-  }
-
-  tags.forEach((tag, index) => {
-    const tagString =
-      index < 1
-        ? tag
-        : tag === "head"
-        ? `${tags[index - 1]}..`
-        : `${tags[index - 1]}..${tag}`;
-
-    const commitsArray = child
-      .execSync(
-        `git log ${tagString} --format=%B----SEPARATOR----%H----DELIMITER---- ${
-          options?.root ?? "./"
-        }`
-      )
-      .toString("utf-8")
-      .split("----DELIMITER----\n")
-      .map((commit) => {
-        const [message, sha] = commit.split("----SEPARATOR----");
-
-        const title = message
-          ?.split("\n")[0]
-          ?.replaceAll(/#([0-9]{1,})/gm, `[#$1](${repo}/issues/$1)`);
-
-        return {
-          message: message ?? "",
-          sha: sha ?? "",
-          title: title ?? "",
-        };
-      })
-      .filter((commit) => Boolean(commit.title) && Boolean(commit.sha));
-
-    const tagDate = child
-      .execSync(`git log -1 --format=%ai ${tag === "head" ? "" : tag}`)
-      .toString("utf-8");
-
-    const store: { text: string; type: storeType }[] = [];
-
-    commitsArray.forEach((commit) => {
-      const formattedCommit = (replace?: string) =>
-        `* ${commit.title.replace(
-          !replace ? "" : new RegExp(`${replace}\\(.*\\): |${replace}: `),
-          ""
-        )} ([${commit.sha.substring(0, 6)}](${repo}/commit/${commit.sha}))\n`;
-
-      let isPushed = false;
-      categories.forEach((cat) => {
-        cat.storeTypes.forEach((st) => {
-          if (new RegExp(`${st}\\(.*\\): |${st}: `).test(commit.title)) {
-            store.push({
-              text: formattedCommit(st),
-              type: commit.message.includes("BREAKING CHANGE")
-                ? "BREAKING CHANGE"
-                : st,
-            });
-            isPushed = true;
-          }
-        });
-      });
-
-      if (!isPushed) {
-        store.push({ text: formattedCommit(), type: "untagged" });
-      }
+  allTags.forEach((tag, index) => {
+    const tagDate = GetTagDate(tag);
+    const commits = GetCommits({
+      from: allTags[index - 1] ?? tag,
+      path: config.src,
+      repo: repo,
+      to: tag,
     });
 
-    let finalChangeLog =
+    if (!commits.length) {
+      return;
+    }
+
+    let changelog =
       tag === "head"
-        ? options?.header ?? ""
+        ? ""
         : `# [${tag}](${repo}/releases/tag/${tag}) (${
             new Date(tagDate).toISOString().split("T")[0] ?? ""
           })\n\n`;
 
-    if (options?.tag?.replace) {
-      finalChangeLog = finalChangeLog.replace(options.tag.replace, "");
+    if (config.replaceTag) {
+      changelog = changelog.replace(config.replaceTag, "");
     }
 
-    categories.forEach((cat) => {
-      const items = store.filter(
-        (r) => cat.storeTypes.includes(r.type) && !ignoreScopes.includes(r.type)
-      );
+    CommitCategories.forEach((cat) => {
+      const items = commits.filter((r) => cat.types.includes(r.type));
 
       if (items.length) {
-        finalChangeLog += `## ${cat.title}\n`;
+        changelog += `## ${cat.title}\n`;
         items.forEach((cm) => {
-          finalChangeLog += cm.text;
+          changelog += cm.title;
         });
-        finalChangeLog += "\n";
+
+        changelog += "\n";
       }
     });
 
-    finalChangeLog += "\n";
+    changelog += "\n";
 
-    if (options?.tag?.onlyStage) {
-      completeChangelog = finalChangeLog;
+    if (config.onlyStage) {
+      content = changelog;
     } else {
-      completeChangelog = finalChangeLog + completeChangelog;
+      content = changelog + content;
     }
   });
 
   fs.writeFileSync(
-    options?.outDir ?? "./CHANGELOG.md",
-    `${prettier.format(completeChangelog, {
+    config.out,
+    `${prettier.format(content, {
       parser: "markdown",
     })}`
   );
 
-  return repo;
+  return { content, repo };
 }
