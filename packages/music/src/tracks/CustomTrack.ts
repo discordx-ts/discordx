b65b893a11a3c323efc88edab494271b036de493
samarmeena <sumarmeena@gmail.com>
2023-02-12 23:23:11 +0530

    refactor(music): tracks classes

--- packages/music/src/tracks/CustomTrack.ts
+++ packages/music/src/tracks/CustomTrack.ts
@@ -2,24 +2,26 @@ import type internal from "node:stream";
 
 import type { AudioResource, StreamType } from "@discordjs/voice";
 import { createAudioResource } from "@discordjs/voice";
-import type { GuildMember, User } from "discord.js";
 
-import type { CommonTrack, Player } from "../index.js";
+import type { CommonTrack } from "../index.js";
 import { Track } from "./Track.js";
+
+/**
+ * Track options
+ */
+export interface CustomTrackItem {
+  [key: string]: any;
+  source: string | internal.Readable;
+  streamType?: StreamType;
+  title: string;
+}
 
 /**
  * Music track
  */
 export class CustomTrack extends Track {
-  constructor(
-    public player: Player,
-    public title: string,
-    public source: string | internal.Readable,
-    public duration?: number,
-    public streamType?: StreamType,
-    public user?: User | GuildMember
-  ) {
-    super(title, undefined, source);
+  constructor(public info: CustomTrackItem) {
+    super(info.title, undefined, info.source);
     // empty constructor
   }
 
@@ -28,9 +30,9 @@ export class CustomTrack extends Track {
    * @returns
    */
   public createAudioResource(): AudioResource<CommonTrack> {
-    return createAudioResource(this.source, {
+    return createAudioResource(this.info.source, {
       inlineVolume: true,
-      inputType: this.streamType,
+      inputType: this.info.streamType,
       metadata: this,
     });
   }
