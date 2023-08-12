1056134695782f551eba3e9113bb4962a93c38c0
samarmeena <sumarmeena@gmail.com>
2023-08-12 06:06:29 +0530

    fix: types

--- packages/importer/index.d.ts
+++ packages/importer/index.d.ts
@@ -1,4 +1,4 @@
 export declare const isESM: boolean;
 export declare function dirname(url: string): string;
-export declare async function resolve(...paths: string[]): string[];
-export declare async function importx(...paths: string[]): Promise<void>;
+export declare function resolve(...paths: string[]): Promise<string[]>;
+export declare function importx(...paths: string[]): Promise<void>;
