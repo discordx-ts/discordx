988baaa008e8b5312a54e2537cef236259897290
samarmeena <sumarmeena@gmail.com>
2024-03-02 02:16:52 +0530

    chore: dep update

--- packages/discordx/src/types/core/literal.ts
+++ packages/discordx/src/types/core/literal.ts
@@ -54,17 +54,17 @@ export type TruncateTo32<T extends string> = T extends ""
     : T;
 
 export type NotEmptyCheck<T> = T extends "" ? never : T;
-export type NotEmpty<T> = NotEmptyCheck<T> extends never
-  ? "A string of zero length is not allowed"
-  : T;
+export type NotEmpty<T> =
+  NotEmptyCheck<T> extends never ? "A string of zero length is not allowed" : T;
 
 export type VName<S extends string> = NotEmptyCheck<S> &
   Lowercase<S> &
   TruncateTo32<S> &
   ForbidCharacter<S, SpecialCharacters>;
 
-export type VerifyName<T extends string> = T extends VName<T>
-  ? T
-  : VName<T> extends never
-    ? "Name must only be lowercase with no space as per Discord guidelines"
-    : VName<T>;
+export type VerifyName<T extends string> =
+  T extends VName<T>
+    ? T
+    : VName<T> extends never
+      ? "Name must only be lowercase with no space as per Discord guidelines"
+      : VName<T>;
