0c2542b2bfd7ea7b8b7660eeaa5201e831ed59c6
samarmeena <sumarmeena@gmail.com>
2024-03-20 21:11:55 +0530

    refactor: license

--- packages/di/src/logic/impl/DefaultDependencyRegistryEngine.ts
+++ packages/di/src/logic/impl/DefaultDependencyRegistryEngine.ts
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------------------------------------------------------------
  * Copyright (c) Vijay Meena <vijayymmeena@gmail.com> (https://github.com/samarmeena). All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
+ * Licensed under the Apache License. See License.txt in the project root for license information.
  * -------------------------------------------------------------------------------------------------------
  */
 import type { InstanceOf } from "../../index.js";
