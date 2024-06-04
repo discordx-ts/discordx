2b8073dde1fd0417ca578800dcd71b78bfc62917
samarmeena <sumarmeena@gmail.com>
2024-06-04 09:22:48 +0530

    refactor: cleanup

--- packages/koa/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * -------------------------------------------------------------------------------------------------------
- * Copyright (c) Vijay Meena <vijayymmeena@gmail.com> (https://github.com/samarmeena). All rights reserved.
- * Licensed under the Apache License. See License.txt in the project root for license information.
- * -------------------------------------------------------------------------------------------------------
- */
-export * from "./common.js";
